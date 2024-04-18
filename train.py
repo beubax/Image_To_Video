73b80d92076d9322ea3c789dac64cb88208822ae
Tejas Ravishankar <beubax@pop-os.localdomain>
2024-04-17 21:53:37 -0400

    batch size fix in evaluate

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from lightning_module import VideoLightningModule
 @click.option("-f", "--frames-per-clip", type=int, default=16, help="frame per clip.")
 @click.option("-v", "--video-size", type=click.Tuple([int, int]), default=(224, 224), help="frame per clip.")
 @click.option("--testing", type=bool, default=False, help="To test functionality.")
-@click.option("--max-epochs", type=int, default=-1, help="max epochs.")
+@click.option("--max-epochs", type=int, default=None, help="max epochs.")
 @click.option("--num-workers", type=int, default=4)
 @click.option("--fast-dev-run", type=bool, is_flag=True, show_default=True, default=False)
 @click.option("--seed", type=int, default=42, help="random seed.")
