2af4dd72d32e90e981d359584657298c7d270304
Tejas Ravishankar <beubax@pop-os.localdomain>
2025-01-02 17:40:45 -0500

    Modify kinetics downloader

--- convert_pt.py
+++ convert_pt.py
@@ -4,44 +4,44 @@ import coremltools as ct
 from vit.vision_transformer_graph import vit_base
 torch.cuda.empty_cache()
 # Instantiate the model and load the checkpoint
-model = vit_base(num_classes=51)
-checkpoint = torch.load("epoch=2.ckpt", map_location="cpu")
+# model = vit_base(num_classes=51)
+# checkpoint = torch.load("epoch=2.ckpt", map_location="cpu")
 
-# Check if the checkpoint contains 'state_dict' or is directly the model weights
-if 'state_dict' in checkpoint:
-    model.load_state_dict(checkpoint['state_dict'], strict=False)
-else:
-    model.load_state_dict(checkpoint, strict=False)
+# # Check if the checkpoint contains 'state_dict' or is directly the model weights
+# if 'state_dict' in checkpoint:
+#     model.load_state_dict(checkpoint['state_dict'], strict=False)
+# else:
+#     model.load_state_dict(checkpoint, strict=False)
 
-print("Model loaded successfully with checkpoint weights.")
-# torch.save(model.state_dict(), "vit_model.pt")
+# print("Model loaded successfully with checkpoint weights.")
+# # torch.save(model.state_dict(), "vit_model.pt")
 
-model.to(torch.device("cuda"))
+# model.to(torch.device("cuda"))
 
-# Save the Lightning model as a .pt file
-example_input_tensor = torch.randn(1, 3, 16, 224, 224)
-example_input_tensor = example_input_tensor.to(torch.device("cuda"))
-output = model(example_input_tensor)
-print(output.shape)
-trace_model = torch.jit.trace(model, example_input_tensor)
-torch.jit.save(trace_model, "vit_model.pt")
-print("Model saved as vit_model.pt")
+# # Save the Lightning model as a .pt file
+# example_input_tensor = torch.randn(1, 3, 16, 224, 224)
+# example_input_tensor = example_input_tensor.to(torch.device("cuda"))
+# output = model(example_input_tensor)
+# print(output.shape)
+# trace_model = torch.jit.trace(model, example_input_tensor)
+# torch.jit.save(trace_model, "vit_model.pt")
+# print("Model saved as vit_model.pt")
 
 
 # Convert .pt model directly to Core ML (.mlpackage) format
-# dummy_input = torch.randn(1, 3, 224, 224)  # Define a dummy input with the correct input shape
+dummy_input = torch.randn(1, 3, 224, 224)  # Define a dummy input with the correct input shape
 
-# # Convert the model using coremltools, specifying the input shape
-# mlmodel = ct.convert(
-#     "vit_model.pt",
-#     source="pytorch",  # Specify source as PyTorch
-#     inputs=[ct.TensorType(shape=dummy_input.shape)],  # Define the input shape for Core ML
-#     convert_to="mlprogram"  # Use mlprogram format for iOS 14+ compatibility
-# )
+# Convert the model using coremltools, specifying the input shape
+mlmodel = ct.convert(
+    "vit_model.pt",
+    source="pytorch",  # Specify source as PyTorch
+    inputs=[ct.TensorType(shape=dummy_input.shape)],  # Define the input shape for Core ML
+    convert_to="mlprogram"  # Use mlprogram format for iOS 14+ compatibility
+)
 
-# # Save the Core ML model
-# mlmodel.save("vit_model.mlpackage")
-# print("Model successfully converted to Core ML format and saved as vit_model.mlpackage.")
+# Save the Core ML model
+mlmodel.save("vit_model.mlpackage")
+print("Model successfully converted to Core ML format and saved as vit_model.mlpackage.")
 
 
 
--- train_test.py
+++ train_test.py
@@ -8,20 +8,17 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import transforms as T
 from torchvision.transforms._transforms_video import ToTensorVideo
 from pytorchvideo.transforms import Normalize, Permute, RandAugment
-from dataset.hmdb51 import HMDB51
+from dataset.kinetics import Kinetics
 from lightning_module import VideoLightningModule
-import fiftyone as fo
-import fiftyone.zoo as foz
-from flash.video import VideoClassificationData, VideoClassifier
 
 @click.command()
-@click.option("-r", "--dataset-root", type=click.Path(exists=True), required=False, help="path to dataset.")
-@click.option("-a", "--annotation-path", type=click.Path(exists=True), required=False, help="path to dataset.")
+@click.option("-r", "--dataset-root", type=click.Path(exists=True), required=True, help="path to dataset.")
+@click.option("-a", "--annotation-path", type=click.Path(exists=True), required=True, help="path to dataset.")
 @click.option("-t", "--resume-training", type=click.Path(exists=True), default=None, help="Checkpoint path to resume training from.")
 @click.option("-p", "--point-cloud", type=bool, default=False, help="Whether to perform point cloud classify or not.")
 @click.option("--trajectory", type=bool, default=False, help="Whether to trajectory temporal modelling.")
 @click.option("-nc", "--num-classes", type=int, default=51, help="num of classes of dataset.")
-@click.option("-b", "--batch-size", type=int, default=16, help="batch size.")
+@click.option("-b", "--batch-size", type=int, default=32, help="batch size.")
 @click.option("-f", "--frames-per-clip", type=int, default=16, help="frame per clip.")
 @click.option("-v", "--video-size", type=click.Tuple([int, int]), default=(224, 224), help="frame per clip.")
 @click.option("--testing", type=bool, default=False, help="To test functionality.")
@@ -71,53 +68,49 @@ def main(
         ]
     )
     
-    # train_metadata_file = "hmdb51-train-meta.pickle"
-    # train_precomputed_metadata = None
-    # if os.path.exists(train_metadata_file):
-    #     with open(train_metadata_file, "rb") as f:
-    #         train_precomputed_metadata = pickle.load(f)
+    train_metadata_file = "kinetics-train-meta.pickle"
+    train_precomputed_metadata = None
+    if os.path.exists(train_metadata_file):
+        with open(train_metadata_file, "rb") as f:
+            train_precomputed_metadata = pickle.load(f)
 
-#     train_set = HMDB51(
-#     root=dataset_root,
-#     annotation_path=annotation_path,
-#     _precomputed_metadata=train_precomputed_metadata,
-#     frames_per_clip=frames_per_clip,
-#     step_between_clips=8,
-#     frame_sample_rate=2,
-#     train=True,
-#     output_format="THWC",
-#     transform=train_transform,
-# )
+    train_set = Kinetics(
+    root=dataset_root,
+    _precomputed_metadata=train_precomputed_metadata,
+    frames_per_clip=frames_per_clip,
+    step_between_clips=8,
+    frame_sample_rate=2,
+    split="train",
+    download=True,
+    output_format="THWC",
+    transform=train_transform,
+)
 
+    if not os.path.exists(train_metadata_file):
+        with open(train_metadata_file, "wb") as f:
+            pickle.dump(train_set.metadata, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-    # if not os.path.exists(train_metadata_file):
-    #     with open(train_metadata_file, "wb") as f:
-    #         pickle.dump(train_set.metadata, f, protocol=pickle.HIGHEST_PROTOCOL)
+    val_metadata_file = "kinetics-val-meta.pickle"
+    val_precomputed_metadata = None
+    if os.path.exists(val_metadata_file):
+        with open(val_metadata_file, "rb") as f:
+            val_precomputed_metadata = pickle.load(f)
 
-    # val_metadata_file = "hmdb51-val-meta.pickle"
-    # val_precomputed_metadata = None
-    # if os.path.exists(val_metadata_file):
-    #     with open(val_metadata_file, "rb") as f:
-    #         val_precomputed_metadata = pickle.load(f)
+    val_set = Kinetics(
+        root=dataset_root,
+        _precomputed_metadata=val_precomputed_metadata,
+        frames_per_clip=frames_per_clip,
+        step_between_clips=8,
+        frame_sample_rate=2,
+        split="val",
+        download=True,
+        output_format="THWC",
+        transform=test_transform,
+    )
 
-    # val_set = HMDB51(
-    #     root=dataset_root,
-    #     annotation_path=annotation_path,
-    #     _precomputed_metadata=val_precomputed_metadata,
-    #     frames_per_clip=frames_per_clip,
-    #     step_between_clips=8,
-    #     frame_sample_rate=2,
-    #     train=False,
-    #     output_format="THWC",
-    #     transform=test_transform,
-    # )
-
-    # if not os.path.exists(val_metadata_file):
-    #     with open(val_metadata_file, "wb") as f:
-    #         pickle.dump(val_set.metadata, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-    train_set = foz.load_zoo_dataset("hmdb51", split="train")
-    test_set = foz.load_zoo_dataset("hmdb51", split="test")
+    if not os.path.exists(val_metadata_file):
+        with open(val_metadata_file, "wb") as f:
+            pickle.dump(val_set.metadata, f, protocol=pickle.HIGHEST_PROTOCOL)
 
     
     train_dataloader = DataLoader(
@@ -130,23 +123,13 @@ def main(
     )
 
     val_dataloader = DataLoader(
-        test_set,
+        val_set,
         batch_size=batch_size,
         num_workers=num_workers,
         shuffle=False,
         drop_last=True,
         pin_memory=True,
     )
-
-    datamodule = VideoClassificationData.from_fiftyone(
-    train_dataset=train_set,
-    predict_dataset=test_set,
-    label_field="ground_truth",
-    batch_size=1,
-    clip_sampler="uniform",
-    clip_duration=1,
-    decode_audio=False,
-)
 
     model = VideoLightningModule(
         num_classes=num_classes,
@@ -175,7 +158,7 @@ def main(
         callbacks=callbacks,
     )
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader, ckpt_path=resume_training)
-    trainer.save_checkpoint("./vvit_hmdb51.ckpt")
+    trainer.save_checkpoint("./kinetics.ckpt")
 
 
 if __name__ == "__main__":
