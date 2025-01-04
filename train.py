7ece45da378016a1bda2f89c3ffa5f5fe222b3ec
Tejas Ravishankar <beubax@pop-os.localdomain>
2025-01-03 20:05:03 -0500

    Train on fold 2

--- train.py
+++ train.py
@@ -81,7 +81,7 @@ def main(
     frames_per_clip=frames_per_clip,
     step_between_clips=8,
     frame_sample_rate=2,
-    fold=2,
+    fold=3,
     train=True,
     output_format="THWC",
     transform=train_transform,
@@ -105,7 +105,7 @@ def main(
         step_between_clips=8,
         frame_sample_rate=2,
         train=False,
-        fold=2,
+        fold=3,
         output_format="THWC",
         transform=test_transform,
     )
